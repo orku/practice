b5f6ecc33407c0f62e176ab88906b296af27753a
orku <morrowind22872@gmail.com>
2020-09-22 01:23:53 +0300

    nice day

--- Grab.cpp
+++ Grab.cpp
@@ -72,6 +72,97 @@ void BaslerCameraGrab(CBaslerUniversalInstantCamera& camera) {
     }
 }
 
+void create_array_of_images(std::ifstream& file, int count_of_images, vector<Mat>& img_array) {
+    if (file.is_open())
+    {
+        for (int i = 0; i < count_of_images; ++i)
+        {
+            char string[20];
+            file.getline(string, 20, '\n');
+            cout << string << i << endl;
+            img_array.push_back(cv::imread(string, CV_8UC3));
+        }
+    }
+    else
+    {
+        std::cerr << "can not open file" << endl;
+    }
+}
+
+void create_known_chessboadr_posiyion(vector<cv::Point3f>& object_corners, float calib_squar_edge_lenght, cv::Size board_sz) {
+    for (int i = 0; i < board_sz.width; i++)
+    {
+        for (int j = 0; j < board_sz.height; j++)
+        {
+            object_corners.push_back(cv::Point3f(i * calib_squar_edge_lenght, j * calib_squar_edge_lenght, 0.0f));
+        }
+    }
+}
+
+int get_chessboard_corners(vector<Mat> img_arr, vector<vector<cv::Point2f>>& found_coreners, cv::Size board_sz) {
+    int count_of_foud_patterns = 0;
+    for (vector<Mat>::iterator i = img_arr.begin(); i != img_arr.end(); i++)
+    {
+        vector<cv::Point2f> bufffer;
+        bool result = cv::findChessboardCorners(*i, board_sz, bufffer);
+        if (result)
+        {
+            ++count_of_foud_patterns;
+            found_coreners.push_back(bufffer);
+            cv::drawChessboardCorners(*i, board_sz, bufffer, result);
+            cv::imshow("sds", *i);
+            cv::waitKey(200);
+        }
+        else
+        {
+            cout << "can not find chessboard" << endl;
+        }
+    }
+    return count_of_foud_patterns;
+}
+
+int get_chessboard_corners(vector<Mat> img_arr, vector<vector<cv::Point2f>>& found_coreners, cv::Size board_sz, int success_cases) {
+    int count_of_foud_patterns = 0;
+    while (count_of_foud_patterns != success_cases)
+    {
+        for (vector<Mat>::iterator i = img_arr.begin(); i != img_arr.end() && count_of_foud_patterns != success_cases; i++)
+        {
+            vector<cv::Point2f> bufffer;
+            bool result = cv::findChessboardCorners(*i, board_sz, bufffer);
+            if (result)
+            {
+                ++count_of_foud_patterns;
+                found_coreners.push_back(bufffer);
+                cv::drawChessboardCorners(*i, board_sz, bufffer, result);
+                cv::imshow("sds", *i);
+                cv::waitKey(200);
+            }
+            else
+            {
+                cout << "can not find chessboard" << endl;
+            }
+        }
+        return count_of_foud_patterns;
+    }
+}
+
+bool get_chessboard_corners_stereo(vector<Mat> img_arr_left, vector<Mat> img_arr_right, vector<vector<cv::Point2f>>& found_coreners_left,
+    vector<vector<cv::Point2f>>& found_coreners_right, cv::Size board_sz, int success_cases_) {
+    int success_cases_left = get_chessboard_corners(img_arr_left, found_coreners_left, board_sz, success_cases_);
+    int success_cases_right = get_chessboard_corners(img_arr_right, found_coreners_right, board_sz, success_cases_);
+    if (success_cases_right == success_cases_left)
+    {
+        cout << "All nice!" << endl;
+        return true;
+    }
+    else
+    {
+        cout << " Something get wrong" << endl;
+        return false;
+    }
+
+}
+
 
 // Limits the amount of cameras used for grabbing.
 // It is important to manage the available bandwidth when grabbing with multiple cameras.
@@ -90,7 +181,7 @@ int main(int argc, char* argv[])
     int exitCode = 0;
     const size_t count_of_camers = 2;
     const float calibration_squar_edge_length = 0.03f; // in meters
-    const cv::Size_<int> board_sz = cv::Size(8, 8);
+    const cv::Size_<int> board_sz = cv::Size(7, 5);
     std::vector<cv::Point2f > corners_1, corners_2;
     std::vector<std::vector<cv::Point2f>> img_points_left, img_points_right;
     std::vector<cv::Point3f> object_corners;
